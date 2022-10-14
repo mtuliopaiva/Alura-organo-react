716c227cf20d1dcf0e2c2aeab3c7b102e20557f1
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-10-14 10:25:52 -0300

    Projeto Organo - Evoluindo com React - Efetuando a criação dos cards

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,15 +6,7 @@ import { useState } from 'react';
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data-Science',
-        'DevOps',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
+
 
     const [nome, setNome] = useState('');
     const [cargo, setCargo] = useState('');
@@ -61,7 +53,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                 obrigatorio={true} 
                 label='Time' 
-                itens ={times}
+                itens ={props.times}
                 valor={time}
                 aoAlterado={valor =>setTime(valor)}
                 />
